37ebc057a259fc5aefa5a0d259692695fc7f2b59
Patrick O'Boyle <patrick.oboyle@meltwater.com>
2022-08-18 14:20:03 -0400

    fix: add docker

---
+++ server/config/local_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export QBIT_HOST=http://localhost:8080
+export STANDALONE_SERVER_PORT=8081
--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const app = express(); // create express app
 const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
 
 app.use(express.static("../release/public"));
 
 app.use(
   "/api",
   createProxyMiddleware({
-    target: process.env.QBIT_HOST,
+    target: process.env.QBIT_HOST || 'http://localhost:8080',
     changeOrigin: true,
     headers: {
-      referer: process.env.QBIT_HOST,
+      referer: process.env.QBIT_HOST || 'http://localhost:8080',
     },
   })
 );
 
-app.listen(parseInt(process.env.STANDALONE_SERVER_PORT), () => {
-  console.log(`server started on port ${process.env.STANDALONE_SERVER_PORT}`);
+app.listen(parseInt(process.env.STANDALONE_SERVER_PORT || '8081'), () => {
+  console.log(`server started on port ${process.env.STANDALONE_SERVER_PORT || '8081'}`);
 });
